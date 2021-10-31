8b477e3da9fea66184c52921d480e4b4fa813594
Sanjeev  Mishra <56467938+sanjeevmishra391@users.noreply.github.com>
2021-10-31 11:04:07 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const userRoute = require('./routes/userRoute');
+const propertyRoute = require('./routes/propertyRoute');
+const cookieParser = require('cookie-parser');
+
+app.use(cors({
+    origin: '*'
+}));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+
+app.use('/api/user', userRoute);
+app.use('/api/property', propertyRoute);
+
+mongoose.connect(
+    'mongodb+srv://root:root123@cluster0.spzqt.mongodb.net/invitationhomesApi?retryWrites=true&w=majority',
+    {useNewUrlParser: true, useUnifiedTopology: true}
+).then(() => {
+    console.log("Connected to database");
+}).catch((err) => {
+    console.log("Can't connect", err);
+});
+
+const PORT = 7000;
+app.listen(PORT, () => {
+    console.log('Listening to port', PORT);
+})
